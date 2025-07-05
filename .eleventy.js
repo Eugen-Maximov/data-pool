e3550b17e8e192c4d44d641e8c464f597e6d2873
e.maximov <e.maximov@qiwi.com>
2025-07-05 11:37:13 +0300

    ver 0.0.1 - fix styles

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("images");
     return {
+        pathPrefix: "/data-pool/",
         dir: {
             input: "content",
             includes: "../templates/includes",
