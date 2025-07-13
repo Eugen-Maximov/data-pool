b77975cad3cf4deb8802ea2f76944d2e3c9ce41c
e.maximov <e.maximov@qiwi.com>
2025-07-13 19:31:44 +0300

    Небольшой фикс скролла сайдбара v3

--- app/app.js
+++ app/app.js
@@ -116,18 +116,6 @@ window.initPageScripts = async function () {
             tocPopup.classList.toggle("hidden");
         });
     }
-
-    // --- Плавный скролл по якорям ---
-//    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-//        anchor.addEventListener("click", function (e) {
-//            e.preventDefault();
-//            const targetId = this.getAttribute("href").slice(1);
-//            const target = document.getElementById(targetId);
-//            if (target) {
-//                target.scrollIntoView({ behavior: "smooth" });
-//            }
-//        });
-//    });
 };
 
 // Запускаем сразу при первой загрузке:
